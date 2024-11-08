68a4c5f63a180509ce6559ed874fe15cfb878dff
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-11-08 13:58:59 +0530

    ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+// Decorator
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'first-angular-app';
+  name: string = 'ujjwal';
+
+  test(): void {
+    // alert(this.name);
+    console.log(this.name);
+  }
 }
