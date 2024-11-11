f5e91f41a1969704e9a251d1839edee784f70d57
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-11-11 07:53:58 +0530

    Setup Routes

--- src/app/component/add-registration/add-registration.component.ts
+++ src/app/component/add-registration/add-registration.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-add-registration',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './add-registration.component.html',
   styleUrl: './add-registration.component.css'
 })
--- src/app/component/list-registration/list-registration.component.ts
+++ src/app/component/list-registration/list-registration.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-list-registration',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './list-registration.component.html',
   styleUrl: './list-registration.component.css'
 })
