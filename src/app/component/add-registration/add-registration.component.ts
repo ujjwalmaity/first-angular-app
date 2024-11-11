7c1ffd2f9b11d786b39884ed34f0e0744cfad8a3
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-11-11 08:36:49 +0530

    Develop form using FormGroup & FormBuilder

--- src/app/component/add-registration/add-registration.component.ts
+++ src/app/component/add-registration/add-registration.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-registration',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, ReactiveFormsModule],
   templateUrl: './add-registration.component.html',
   styleUrl: './add-registration.component.css'
 })
-export class AddRegistrationComponent {
+
+export class AddRegistrationComponent implements OnInit {
+  myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobile: 0,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 
 }
